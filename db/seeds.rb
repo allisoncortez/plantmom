048d3ff93ff6c2a7bab09ec26c59eb751bc607c0
allison cortez <allizetorc@gmail.com>
2020-02-07 22:09:19 +0000

    updating seed file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+#Demo 2 users
+
+allison = User.create(name: "Allison", email: "allizetorc@gmail.com", password: "password1")
+barb = User.create(name: "Barbara", email: "barb@gmail.com", password: "password2") 
+
+Plant.create(name:"name", soil:"brown and grainy", sun_light: "a lot", water:"2 times a month, 1 teaspoon", user_id: allison.id)
+
+allisons_first_plant = allison.plant.build(name: "dead_plant")
+
+
+
+# t.string "name"
+#     t.string "soil"
+#     t.string "sun_light"
+#     t.string "water"
+#     t.integer "user_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
