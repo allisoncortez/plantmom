40b05be6716b2170067f3284af685b000412b8de
allison cortez <allizetorc@gmail.com>
2020-02-08 20:21:46 +0000

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,14 @@
 allison = User.create(username: "Allison", email: "allizetorc@gmail.com", password: "password1")
 barb = User.create(username: "Barbara", email: "barb@gmail.com", password: "password2") 
 
-Plant.create(name:"name", soil:"brown and grainy", sun_light: "a lot", water:"2 times a month, 1 teaspoon", user_id: allison.id)
-
-allisons_first_plant = allison.plants.build(name: "dead_plant")
+Plant.create(name:"Sunflower", description: "plant description here. Requires lots of light. Water twice a week.", care_level:"medium", user_id: allison.id)
 
 
+#use AR to pre-associate data 
+allison.plants.create(name:"Flower", description: "plant description here.", care_level:"minimal")
 
-# t.string "name"
-#     t.string "soil"
-#     t.string "sun_light"
-#     t.string "water"
-#     t.integer "user_id"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
+
+barbs_first_plant = barb.plants.build(name:"Sad Plant", description: "plant description here.", care_level:"expert")
+barbs_first_plant.save
+
+
