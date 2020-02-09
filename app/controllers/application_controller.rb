56d1955f30173b9ab7c0c09dbbd44d4a8b5d2329
allison cortez <allizetorc@gmail.com>
2020-02-09 15:10:10 +0000

    updates to app controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,22 @@ class ApplicationController < Sinatra::Base
     
     def current_user 
       #should return current user, if there is one 
-      @current_user ||= User.find_by(id: session[:user_id])
+      # @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
+    
+    def login(username, password)
+    user = User.find_by(:username => username) 
+     if user && user.authenticate(password)
+      session[:user_id] = user.id
+     else
+      redirect "/users/login"
+     end
+   end
+   
+   def logout!
+    session.clear
+   end
   end
 
 end
