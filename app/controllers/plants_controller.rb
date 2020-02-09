25d0d016021089df30bc9a1887901e22751749e7
allison cortez <allizetorc@gmail.com>
2020-02-09 16:34:38 +0000

    updates to instance methods in plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,7 +1,7 @@
 class PlantsController < ApplicationController
   
   get '/plants' do 
-    @plant_entry = Plant.all 
+    @plant = Plant.all 
     erb :"/plants/index"
   end
    
@@ -11,49 +11,46 @@ class PlantsController < ApplicationController
   
   #show route for plants
   get '/plants/:id' do 
-      @plant_entry = Plant.find_by(id: params[:id])
+      @plant = Plant.find_by(id: params[:id])
       erb :'/plants/show'
   end
   
   #sends us to edit.erb which will render an edit form
   get '/plants/:id/edit' do 
-    @plant_entry = Plant.find_by(id: params[:id])
-    if logged_in?
-    if authorized_to_edit?(@plant_entry)
+    @plant = Plant.find_by(id: params[:id])
       erb :'/plants/edit'
-    else 
-      redirect "users/#{current_user.id}"
-    end
-  else 
-    redirect '/'
-  end
   end
   
 
   #post route for new plant entries
   post '/plants' do 
-    if !logged_in? 
-      redirect '/'
-    end 
-  
-    #only save IF it has all params
-    if params[:description] != ""
-      @plant_entry = Plant.create(name: params[:name], description: params[:description], care_level: params[:care_level],  user_id: current_user.id)
-    
-      redirect "/plants/#{@plant_entry.id}"
+    @plant = Plant.new(params[:plant])
+    if valid_params? && @plant.save 
+      redirect "/plants/#{@plant.id}"
     else 
-      redirect '/plants/new'
+      redirect "/plants/new"
+      
+    # if !logged_in? 
+    #   redirect '/'
+    # end 
+  
+    # #only save IF it has all params
+    # if params[:description] != ""
+    #   @plant_entry = Plant.create(name: params[:name], description: params[:description], care_level: params[:care_level],  user_id: current_user.id)
+    
+    #   redirect "/plants/#{@plant_entry.id}"
+    # else 
+    #   redirect '/plants/new'
     end
-
   end
 
   
   patch '/plants/:id' do 
-    @plant_entry = Plant.find_by(id: params[:id])
-    if valid_params? && @plant_entry.update(params[:plant]) 
-      redirect "/plants/#{@plant_entry.id}"
+    @plant = Plant.find_by(id: params[:id])
+    if valid_params? && @plant.update(params[:plant]) 
+      redirect "/plants/#{@plant.id}"
     else
-        redirect "/plants/#{@plant_entry.id}/edit"
+        redirect "/plants/#{@plant.id}/edit"
       end
   end
   
