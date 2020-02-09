a150c6b402240b14f10faacc41dfc7a46bc60c78
allison cortez <allizetorc@gmail.com>
2020-02-09 01:34:36 +0000

    edit plants route added

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,36 +1,41 @@
 class PlantsController < ApplicationController
 
-#get plants new to creat new plant entries 
-get '/plants/new' do 
-  erb :'/plants/new'
-end
-
-#post route for new plant entries
-post '/plants' do 
-#create new plant and save to DB 
-#only if user is logged in 
-  if !logged_in? 
-    redirect '/'
-  end 
-  
-  #only save IF it has all params
-  if params[:description] != ""
-    @plant_entry = Plant.create(name: params[:name], description: params[:description], care_level: params[:care_level],  user_id: current_user.id)
-    
-    redirect "/plants/#{@plant.id}"
-  else 
-    redirect '/plants/new'
+  #get plants new to creat new plant entries 
+  get '/plants/new' do 
+    erb :'/plants/new'
   end
 
-end
+  #post route for new plant entries
+  post '/plants' do 
+  #create new plant and save to DB 
+  #only if user is logged in 
+    if !logged_in? 
+      redirect '/'
+    end 
+  
+    #only save IF it has all params
+    if params[:description] != ""
+      @plant_entry = Plant.create(name: params[:name], description: params[:description], care_level: params[:care_level],  user_id: current_user.id)
+    
+      redirect "/plants/#{@plant.id}"
+    else 
+      redirect '/plants/new'
+    end
+
+  end
 
 
-#show route for plants
-get '/plants/:id' do 
-  @plant_entry = Plant.find(params[:id])
-  erb :'/plants/show'
-end
+  #show route for plants
+  get '/plants/:id' do 
+    @plant_entry = Plant.find(params[:id])
+    erb :'/plants/show'
+  end
 
-#index route for all plants
+  #sends us to edit.erb which will render an edit form
+  get '/plants/:id/edit' do 
+    erb :'/plants/edit'
+  end
+
+  #index route for all plants
 
 end
