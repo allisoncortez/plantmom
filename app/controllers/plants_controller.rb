9294d196636eaac16295ff8cd493521fc2ab69bf
allison cortez <allizetorc@gmail.com>
2020-02-09 00:46:38 +0000

    post plants route logic updated

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,6 +6,24 @@ get '/plants/new' do
 end
 
 #post route for new plant entries
+post '/plants' do 
+#create new plant and save to DB 
+#only if user is logged in 
+  if !logged_in 
+    redirect '/'
+  end 
+  
+  #only save IF it has all params
+  if params[:description] != ""
+    @plant_entry = Plant.create(name: params[:name], description: params[:description], care_level: params[:care_level],  user_id: current_user.id)
+    
+    redirect "/plants/#{@plant.id}"
+  else 
+    redirect '/plants/new'
+  end
+
+end
+
 
 #show route for plants
 
