da3a0457714f5e730b77dd7df09ff162ac5c3da0
allison cortez <allizetorc@gmail.com>
2020-02-20 01:08:51 +0000

    validates user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_many :plants 
   has_secure_password
   
-  validates_presence_of :email, :password 
+  validates :email, uniqueness: true 
+  validates :password, uniqueness: true
 end
