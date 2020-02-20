4d3e25007fc8b0e09934ddc27502b9048017fa9a
allison cortez <allizetorc@gmail.com>
2020-02-20 01:22:44 +0000

    update signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,12 @@ class UsersController < ApplicationController
       redirect '/users/signup'
     else 
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
-      session[:user_id] = @user.id
-    
-      redirect "/users/show/#{current_user.id}"
+      if @user.save
+        session[:user_id] = @user.id
+        redirect "/users/show/#{current_user.id}"
+      else
+        redirect '/users/signup'
+      end
     end
   end
   
